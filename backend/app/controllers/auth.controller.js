e27d87e98221e7a8c8b7762919e727466e2ae6b6
Avnish Yadav <avnishy84@gmail.com>
2022-03-16 11:34:11 +0530

    Signin Token Added

--- backend/app/controllers/auth.controller.js
+++ backend/app/controllers/auth.controller.js
@@ -28,7 +28,7 @@ exports.signup = (req, res) => {
           });
         });
       } else {
-        // user role = 1
+        // user role = 1..
         user.setRoles([1]).then(() => {
           res.send({ message: "User was registered successfully!" });
         });
